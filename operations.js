2914f2afc602e6a158b517adb6978d9aa19f5584
starcom89 <34595672+starcom89@users.noreply.github.com>
2017-12-20 20:09:52 +0300

    Update operations.js

--- operations.js
+++ operations.js
@@ -4,6 +4,26 @@ function sum(a, b) {
   return a + b;
 }
 
+function dif(a, b) {
+    return a - b;
+}
+
 module.exports = {
     sum,
+};
+
+function dif(a, b) {
+    return a - b;
+}
+
+function prod(a, b) {
+    return a * b;
+}
+
+function rat(a, b) {
+    return a / b;
+}
+
+module.exports = {
+    sum, dif, prod, rat,
 };
